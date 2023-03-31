5e509c12494b69a94ceb69d0adf721c063536405
Peter Sun <hs859@cornell.edu>
2023-03-31 18:21:19 -0400

    Add dependency path.

---
+++ parse.py
@@ -0,0 +1,105 @@
+# parse pyproject.toml information
+import tomli
+import sys
+import re
+
+
+def caret_version(version):
+    """Parse a caret version string and return the dependency version."""
+
+    regex_version = r"(\d+).?([0-9.*]+)?$"
+    version_match = re.match(regex_version, version)
+    major_version = version_match.group(1)
+    minor_version = version_match.group(2)
+
+    if int(major_version) > 0 or minor_version is None:
+        upper_version = int(major_version) + 1
+        version_str = f">={version} <{upper_version}.0.0"
+    else:
+        version_match = re.match(regex_version, minor_version)
+        major_minor_version = version_match.group(1)
+        minor_minor_version = version_match.group(2)
+
+        if int(major_minor_version) > 0 or minor_minor_version is None:
+            upper_version = int(major_minor_version) + 1
+            version_str = f">={version} <0.{upper_version}.0"
+        else:
+            upper_version = int(minor_minor_version) + 1
+            version_str = f">={version} <0.0.{upper_version}"
+
+    return version_str
+
+
+def modify_string(version_string):
+    """Modify a string to be compatible with pip.
+
+    The modification is to be compatible with poetry's dependency
+    version format. For caret ("^") and equal ("=") versions. For "~"
+    versions, they are replaced by "~=". For >, <, >=, <=, they are
+    kept as is.
+    """
+    if version_string.startswith("~"):
+        regex_tilde = r"^~=?\s*([0-9.*]+)$"
+        replace_tilde = r"~=\1"
+        return re.sub(regex_tilde, replace_tilde, version_string)
+
+    if version_string.startswith("^"):
+        regex_caret = r"\^\s*([0-9.*]+)$"
+        caret_match = re.match(regex_caret, version_string)
+        return caret_version(caret_match.group(1))
+
+    regex = r"^=?\s*([0-9.*]+)$"
+    replace = r"==\1"
+    dep_str = re.sub(regex, replace, version_string)
+    return dep_str
+
+
+def parse_dependency_dict(dependency_dict):
+    """Parse a dependency dictionary and return a string of dependencies."""
+
+    dep_list = []
+    for key, value in dependency_dict.items():
+        if key == "python":
+            continue
+
+        if isinstance(value, dict): # for optional entries
+            value = value["version"]
+
+        # the value should be a string
+        modified_value = modify_string(value.strip())
+        dep_list.append(f"{key}{modified_value}")
+
+    return dep_list
+
+
+def parse_pyproject(pyproject_data, dependency_path):
+    """Parse a pyproject.toml value and return a string of dependencies.
+
+    For poetry style dependencies, the dependency version is adjusted
+    to be compatible with pip. For example, "^1.0.0" is converted to "==1.0.0".
+
+    For setuptools style dependencies, the dependency version is kept as is.
+    """
+
+    d_path = dependency_path.split(".")
+    data = pyproject_data  # not a copy
+    for dep in d_path:
+        # extract the dependency dictionary
+        data = data[dep]
+
+    if isinstance(data, list):
+        dep_list = data
+    else:
+        dep_list = parse_dependency_dict(data)
+
+    return " ".join(dep_list)
+
+
+if __name__ == "__main__":
+
+    project_path = sys.argv[1]
+    dependency_path = sys.argv[2]
+
+    pip_deps = parse_pyproject(project_path, dependency_path)
+
+    sys.stdout.write(f"::set-output name=DEP::{pip_deps}")
---
+++ test_parse.py
@@ -0,0 +1,106 @@
+import pytest
+from textwrap import dedent
+import tomli
+from parse import parse_dependency_dict, parse_pyproject, modify_string, caret_version
+
+
+@pytest.mark.parametrize(
+    "input_string, expected_output",
+    [
+        ("1.2.3", ">=1.2.3 <2.0.0"),
+        ("1.2", ">=1.2 <2.0.0"),
+        ("1", ">=1 <2.0.0"),
+        ("0.2.3", ">=0.2.3 <0.3.0"),
+        ("0.0.3", ">=0.0.3 <0.0.4"),
+        ("0.0", ">=0.0 <0.1.0"),
+        ( "0", ">=0 <1.0.0")
+    ],
+)
+def test_caret_version(input_string, expected_output):
+    """Test caret_version() function."""
+    assert caret_version(input_string) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_string, expected_output",
+    [
+        ("1", "==1"),
+        ("1.0", "==1.0"),
+        ("1.0.0", "==1.0.0"),
+        ("=1.0.0", "==1.0.0"),
+        ("= 10.11.12", "==10.11.12"),
+        # space in between
+        ("=  1.0.0", "==1.0.0"),
+        # caret
+        ("^ 1.0.0", ">=1.0.0 <2.0.0"),
+        # tilde
+        ("~1.0.0", "~=1.0.0"),
+        ("~=1.0.0", "~=1.0.0"),
+        # wildcard
+        ("*", "==*"),
+        ("2.*", "==2.*"),
+        ("2.0.*", "==2.0.*"),
+        # remains the same
+        ("==1.0.0", "==1.0.0"),
+        (">1.0.0", ">1.0.0"),
+        ("<1.0.0", "<1.0.0"),
+        ("<=1.0.0", "<=1.0.0"),
+        (">=1.0.0", ">=1.0.0"),
+        ("> 1.0.0 < 2.0.0", "> 1.0.0 < 2.0.0"),
+        (">= 1.0.0 < 2.0.0", ">= 1.0.0 < 2.0.0"),
+    ],
+)
+def test_modify_string(input_string, expected_output):
+    assert modify_string(input_string) == expected_output
+
+
+@pytest.fixture
+def pyproject_value():
+    """Return a sample pyproject.toml value."""
+
+    pyproject_toml = """\
+    [tool.poetry.dependencies]
+    python = ">=3.8"
+    tomli = ">=2.0.0"
+    tox = { version = "=3.24.5", optional = true }
+    tox-gh-actions = { version = "==2.10.0", optional = true }
+    sphinx = { version = "^6.1.3", optional = true }
+    sphinx-rtd-theme = { version = " ~ 1.0.0", optional = true }
+
+    [project]
+    dependencies = [
+        "docutils",
+        "BazSpam == 1.1",
+    ]
+    """
+
+    return tomli.loads(dedent(pyproject_toml))
+
+
+def test_parse_dependency_dict(pyproject_value):
+    """Test parse_dependency_dict() function."""
+
+    dependency_dict = pyproject_value["tool"]["poetry"]["dependencies"]
+    expected = [
+        "tomli>=2.0.0",
+        "tox==3.24.5",
+        "tox-gh-actions==2.10.0",
+        "sphinx>=6.1.3 <7.0.0",
+        "sphinx-rtd-theme~=1.0.0",
+    ]
+
+    assert parse_dependency_dict(dependency_dict) == expected
+
+
+def test_parse_pyproject(pyproject_value):
+    """Test parse_pyproject() function."""
+
+    expected_poetry = (
+        "tomli>=2.0.0 tox==3.24.5 tox-gh-actions==2.10.0 "
+        "sphinx>=6.1.3 <7.0.0 sphinx-rtd-theme~=1.0.0"
+    )
+    expected_stanard = "docutils BazSpam == 1.1"
+    assert (
+        parse_pyproject(pyproject_value, "tool.poetry.dependencies") == expected_poetry
+    )
+    assert parse_pyproject(pyproject_value, "project.dependencies") == expected_stanard
